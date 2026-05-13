283ced1a5aa79c85f8e45105f0351dad4e430213
YanYan Chan <yly80103@gmail.com>
2026-05-13 20:53:04 +0700

    Initial Commit

---
+++ MovieMultiModuleApp/MovieMultiModuleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMultiModuleAppApp.swift
+//  MovieMultiModuleApp
+//
+//  Created by mac on 2026-05-13.
+//
+
+import SwiftUI
+
+@main
+struct MovieMultiModuleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
